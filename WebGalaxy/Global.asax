83aebb6ee40780f87c5eb9d158c7fac1b6716845
Wantedy <wanted.kaiser228@gmail.com>
2017-06-28 22:59:22 +0300

    Add project files.

---
+++ WebGalaxy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebGalaxy.MvcApplication" Language="C#" %>
---
+++ WebGalaxy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebGalaxy.Startup))]
+namespace WebGalaxy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
