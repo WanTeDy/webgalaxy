84b89266c97896be894acfae9245c55f5f369576
Wantedy <wanted.kaiser228@gmail.com>
2017-07-10 21:01:24 +0300

    english version

---
+++ WebGalaxy/Areas/en/enAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace WebGalaxy.Areas.en
+{
+    public class enAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "en";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "en_default",
+                "en/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "WebGalaxy.Areas.en.Controllers" }
+
+            );
+        }
+    }
+}
